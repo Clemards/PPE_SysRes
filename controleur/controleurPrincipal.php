a87921ea18685f6af0768ccb2744d16c74531232
ClementArlandis <clement@hemka.com>
2019-11-24 10:08:40 +0100

    CSS

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -1,22 +1,22 @@
 <?php
-
-function controleurPrincipal($action){
+ 
+ function controleurPrincipal($action) {
     $lesActions = array();
     $lesActions["defaut"] = "accueil.php";
-    $lesActions["liste"] = "listeSalles.php";
     $lesActions["connexion"] = "connexion.php";
-    $lesActions["deconnexion"] = "deconnexion.php";
-    $lesActions["detail"] = "detailSalle.php";
+    $lesActions["liste"] = "listeSalles.php";
     $lesActions["inscription"] = "inscription.php";
-
-
-    if (array_key_exists ( $action , $lesActions )){
+    $lesActions["deconnexion"] = "deconnexion.php";
+    $lesActions["profil"] = "profil.php";
+    $lesActions["detail"] = "accueil.php";
+ 
+    if (array_key_exists($action, $lesActions)) {
+        $action = (isset($_GET["action"])) ? $_GET["action"] : $action = (!isset($_SESSION)) ? "connexion" : "defaut";
         return $lesActions[$action];
     }
-    else{
+    else {
         return $lesActions["defaut"];
     }
-
 }
+ 
 
-?>
--- index.php
+++ index.php
@@ -1,27 +1,11 @@
 <!-- Copyright Clément Arlandis, 2019 -->
 
 <?php
-include "getRacine.php";
-include "$racine/controleur/controleurPrincipal.php";
-
-if (isset($_GET["action"])) {
-    $action = $_GET["action"];
-}
-else {
-    if(!isset($_SESSION)){
-        $action = "connexion";
-    } else {
-        $action = "defaut";
-    }
-}
-
-//$action = (isset($_GET["action"])) ? $_GET["action"] : $action = (!isset($_SESSION)) ? "connexion" : "defaut";
-
-if(isset($_SESSION)){
-   echo $_SESSION;
-}
-echo $action;
-
-$fichier = controleurPrincipal($action);
-include "$racine/controleur/$fichier";
-?>
+  include "getRacine.php";
+  include "$racine/controleur/controleurPrincipal.php";
+ 
+  $action = (isset($_GET["action"])) ? $_GET["action"] : $action = (!isset($_SESSION)) ? "connexion" : "defaut";
+ 
+  $fichier = controleurPrincipal($action);
+  include "$racine/controleur/$fichier";
+  ?>
